8cae385bf53a893255ec7fa8e7aaeb533adb4937
Claire Rieusset <claireriussetcr@gmail.com>
2022-06-08 19:04:02 +0200

    First commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <News :apiKey="apiKey" />
+  </div>
+</template>
+
+<script>
+import News from './components/News'
+
+export default {
+  name: 'App',
+  components: {
+    News,
+  },
+  data() {
+    return {
+      apiKey: '7227efae15504d9db9551ff09a2c8461',
+    }
+  },
+}
+</script>
+
+<style>
+body {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  color: #2c3e50;
+  background-image: url('./assets/Asset 2 3.png');
+  background-repeat: no-repeat;
+
+}
+
+
+</style>
