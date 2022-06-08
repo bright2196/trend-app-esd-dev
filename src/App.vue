c83606e0c0212513105449992a64623b5e461bc5
Claire Rieusset <claireriussetcr@gmail.com>
2022-06-08 19:20:00 +0200

    correction build

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,6 @@
+
 <template>
+
   <div id="app">
     <News :apiKey="apiKey" />
   </div>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/VueJS-NewsAPI/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/Trend-app/" : "/",
 };
 
